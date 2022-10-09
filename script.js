4d9e06698f8805bb580be56072b61e8f9726dfc6
EJ-David <daviderhabor.de@gmail.com>
2022-10-09 13:45:04 +0100

    nn

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let guessRange = [1,2];
+let numOfPoints = 0;
+
+let start = false; // to control html body, the conditional rendering of the start button or the game page
+//k = the correct guess, K-index = the position of the correct guess on the array.
 function askName () {
     let username = sessionStorage.getItem('username');
 
@@ -13,15 +18,28 @@ function askName () {
     console.log(username)
 }
 
+
+
 function points(rangeWidth){
-    let numOfPoints = 0;
     document.getElementById('userpoints').innerHTML = numOfPoints
-    console.log(numOfPoints)
+    // console.log(numOfPoints)
+    // console.log(Math.floor(Math.random() * guessRange.length ))
+    guessRange.push(guessRange[guessRange.length - 1] + 1)
+    let kIndex = Math.floor(Math.random() * guessRange.length)
+    let k = guessRange[kIndex]
+
+    let userGuess = document.getElementById('userguess').value;//listen for onKyPress to know when user has guess
+    console.log(guessRange)
+    console.log(k)
+    console.log(userGuess == k)
 }
 
 
 
 const score = document.querySelector('.btn')
-score.addEventListener("click", points)
+document.getElementById('guessrange').innerHTML =  `${guessRange[0]} ${guessRange[guessRange.length - 1]}`
+
+window.addEventListener("click", points)
+// console.log(guessRange)
 
 
