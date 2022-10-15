59b2546091e93c239dd450a682476436d1d4b130
EJ-David <daviderhabor.de@gmail.com>
2022-10-15 03:06:02 +0100

    minor refactor

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 let guessRange = [1, 2];
-let numOfPoints = 0;
-let inPlay = false;
-let stageNum = 1;
+let numOfPoints = document.getElementById("userpoints").innerHTML;
+let stageNum = document.getElementById("userstage").innerHTML;
+numOfPoints = 0
+stageNum = 1
+var inPlay;
 let form = document.getElementById("form");
+let startDisplay = '<button id="start-btn">yolo</button>';
+let start = document.getElementById("start-btn");
+let end = document.getElementById("end-btn");
 
 // to control html body, the conditional rendering of the start button or the game page
 // k = the correct guess, K-index = the position of the correct guess on the array.
+
+const gameStart = () => {
+  inPlay = true;
+  document.getElementById("game-body").hidden = false;
+  start.hidden = true;
+};
+
+const gameEnd = () => {
+  winOrLose(numOfPoints, stageNum, inPlay);
+  inPlay = !inPlay;
+  stageNum = 1;
+  numOfPoints = 0;
+  document.getElementById("game-body").hidden = true;
+  start.hidden = false;
+}
+
+start.addEventListener("click", gameStart);
+end.addEventListener("click", gameEnd);
+
 function askName() {
   let username = sessionStorage.getItem("username");
   if (username === null) {
@@ -34,29 +58,23 @@ const guessCheck = () => {
   let userInput = document.getElementById("userInput").value;
   let kIndex = Math.floor(Math.random() * guessRange.length);
   let k = guessRange[kIndex];
-  console.log('k is ', k, 'and user guessed ', userInput )
+  console.log("k is ", k, "and user guessed ", userInput);
   if (userInput != null && k == Number(userInput)) {
     numOfPoints++;
-    document.getElementById("userpoints").innerHTML = numOfPoints;
-
     stageNum++;
-    document.getElementById("userstage").innerHTML = stageNum;
-
     guessRange.push(guessRange[guessRange.length - 1] + 1);
     inPlay = true;
-    hint()
+    hint();
   } else {
     inPlay = false;
   }
 
-  form.reset()
+  form.reset();
   winOrLose(numOfPoints, stageNum, inPlay);
-}
+};
 
 const refreshPage = () => {
-  window.location.reload()
-}
+  window.location.reload();
+};
 
-
-form.addEventListener('submit', guessCheck)
-
+form.addEventListener("submit", guessCheck);
