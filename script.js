71adb2523256e72a98c98ae98f6c7509f61beca5
EJ-David <daviderhabor.de@gmail.com>
2022-10-17 21:40:40 +0100

    update

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 let guessRange = [1, 2];
-let userName = document.getElementById('userName')
-
-
-console.log(userName)
-function getName(){
-  let username = sessionStorage.getItem("username");
-  if(userName !== null){
-    document.getElementById('dis-username').innerHTML = userName
-  }
-}
