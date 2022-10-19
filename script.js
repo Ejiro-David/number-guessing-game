38ae7428af1ada55020d0524bdb167b787e4d2cc
EJ-David <daviderhabor.de@gmail.com>
2022-10-19 23:52:33 +0100

    set name function done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let guessRange = [1, 2];
+let userName;
+
+const setNameStore = () => {
+    userName = document.getElementById('userName').value
+    sessionStorage.setItem("username", userName);
+}
+
+ userName = sessionStorage.getItem('username')
+ document.getElementById('dp-username').innerHTML = userName;
+ 
