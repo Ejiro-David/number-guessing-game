a0e529f2d03c0f3eb3dbc0a4e6756146e1a435d1
EJ-David <daviderhabor.de@gmail.com>
2022-10-20 00:10:18 +0100

    guessChecker function built

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ const setNameStore = () => {
  userName = sessionStorage.getItem('username')
  document.getElementById('dp-username').innerHTML = userName;
  
+ const setHint = () => {
+    let firstNum = guessRange[0];
+    let lastNum = guessRange[guessRange.length - 1]
+
+    document.getElementById('guessrange').innerHTML = `${firstNum} thru ${lastNum}`
+ }
+
+
+ const guessCheck = () => {
+    let userInput = document.getElementById("userInput").value;
+    let kIndex = Math.floor(Math.random() * guessRange.length);
+    let k = guessRange[kIndex];
+    console.log(userInput, k)
+    console.log("k is ", k, "and user guessed ", userInput);
+    if (userInput != null && k == Number(userInput)) {
+      numOfPoints++;
+      stageNum++;
+      guessRange.push(guessRange[guessRange.length - 1] + 1);
+      inPlay = true;
+      hint();
+    } else {
+      inPlay = false;
+    }
+  
+    form.reset();
+    winOrLose(numOfPoints, stageNum, inPlay);
+  };
+
+
+document.getElementById('userGuessed').addEventListener('submit', guessCheck)
