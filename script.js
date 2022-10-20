a378bd4e199a0d6666788160e3599a398adb4c03
EJ-David <daviderhabor.de@gmail.com>
2022-10-20 01:01:08 +0100

    bug fixes

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
 let guessRange = [1, 2];
-var userPoints = 0
+var userPoints = 0;
 var stageNum = 1;
-document.getElementById('dp-userstage').innerHTML = stageNum;
 let userName;
 var inPlay;
 
 const setNameStore = () => {
-    userName = document.getElementById('userName').value
-    sessionStorage.setItem("username", userName);
-}
+  userName = document.getElementById("userName").value;
+  sessionStorage.setItem("username", userName);
+};
 
- userName = sessionStorage.getItem('username')
- document.getElementById('dp-username').innerHTML = userName;
- 
- const setHint = () => {
-    let firstNum = guessRange[0];
-    let lastNum = guessRange[guessRange.length - 1]
+userName = sessionStorage.getItem("username");
+document.getElementById("dp-username").innerHTML = userName;
 
-    document.getElementById('guessrange').innerHTML = `${firstNum} thru ${lastNum}`
- }
+const setHint = () => {
+  let firstNum = guessRange[0];
+  let lastNum = guessRange[guessRange.length - 1];
 
+  document.getElementById(
+    "guessrange"
+  ).innerHTML = `${firstNum} thru ${lastNum}`;
+};
 
- const guessCheck = () => {
-    let userInput = document.getElementById("userInput").value;
-    let kIndex = Math.floor(Math.random() * guessRange.length);
-    let k = guessRange[kIndex];
-    console.log("k is ", k, "and user guessed ", userInput);
-    if (userInput != null && k == Number(userInput)) {
-    document.getElementById('dp-userpoints').innerHTML = userPoints++;
-      stageNum++;
-      guessRange.push(guessRange[guessRange.length - 1] + 1);
-      inPlay = true;
-      document.getElementById('winOrLose').innerHTML = 'Correct! To the next'
-      setHint();
-    } else {
-      inPlay = false;
-      document.getElementById('winOrLose').innerHTML = 'Wrong. Try again.'
-    }
-  
-    form.reset();
-  };
+const guessCheck = () => {
+  let userInput = document.getElementById("userInput").value;
+  let kIndex = Math.floor(Math.random() * guessRange.length);
+  let k = guessRange[kIndex];
+  console.log("k is ", k, "and user guessed ", userInput);
+  if (userInput != null && k == Number(userInput)) {
+    document.getElementById("dp-userpoints").innerHTML++;
+    document.getElementById("dp-userstage").innerHTML++;
+    guessRange.push(guessRange[guessRange.length - 1] + 1);
+    inPlay = true;
+    document.getElementById("winOrLose").innerHTML = "Correct! To the next";
+    setHint();
+  } else {
+    inPlay = false;
+    document.getElementById("winOrLose").innerHTML = "Wrong. Try again.";
+  }
+  document.getElementById("guessForm").reset();
+};
 
-let form = document.getElementById('guessForm')
-form.addEventListener('click', guessCheck)
-
-
+document.getElementById("guessButton").addEventListener("click", guessCheck);
